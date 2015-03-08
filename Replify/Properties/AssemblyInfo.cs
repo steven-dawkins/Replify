d3483398d140341d0f6ae8f42ed2a12e6c7bbbb6
Steven Dawkins <steven.dawkins@gmail.com>
2015-03-08 20:25:40 +0000

    Added nuspec

--- Replify/Properties/AssemblyInfo.cs
+++ Replify/Properties/AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Replify")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
+
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,3 +33,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.*")]
